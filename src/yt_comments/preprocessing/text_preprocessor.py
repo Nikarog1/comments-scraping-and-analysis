94ad0f07fdf14dbbdb8948f15d956f72b40e92d7
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-02-15 17:22:23 +0100

    Simple text preprocessor

---
+++ src/yt_comments/preprocessing/text_preprocessor.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import re
+import unicodedata
+
+
+
+# compiling just once outside of the main class
+_URL_RE = re.compile(r"http?://\+S|www\.\S+", flags=re.IGNORECASE) # if people send url in comms; found during testing some videos
+_WS_RE = re.compile(r"\s+")
+
+class TextPreprocessor:
+    """
+    General text normalization for silver layer
+    """
+    
+    def __init__(self, *, replace_urls_with: str = "<URL") -> None:
+        self._replace_urls_with = replace_urls_with
+    
+    def clean(self, text: str) -> str:
+        text = unicodedata.normalize("NFKC", text)
+        text = _URL_RE.sub(self._replace_urls_with, text)
+        text = text.lower()
+        text = _WS_RE.sub(" ", text).strip()
+        
+        return text
