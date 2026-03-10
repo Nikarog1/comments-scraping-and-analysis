a4e27e32b3aeeb8abb68574a8b82abf26c43be64
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-03-10 17:25:49 +0100

    Add global corpus folder and its frozen parameters in contract.py

---
+++ src/yt_comments/analysis/corpus/contract.py
@@ -0,0 +1,4 @@
+# Created to freeze the logic of computing the gold v3. In other words, some kind of config parameters
+CORPUS_ARTIFACT_VERSION = "corpus_v1"
+CORPUS_DOCUMENT_UNIT = "video"
+CORPUS_INPUT_LAYER = "silver"
