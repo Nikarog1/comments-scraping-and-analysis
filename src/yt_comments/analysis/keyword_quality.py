98fb5d188b256b6915622d99f2e13633d13b32e8
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-03-11 13:49:55 +0100

    Add keyword_quality.py to unfilter sentiment words from the final score in unigrams

---
+++ src/yt_comments/analysis/keyword_quality.py
@@ -0,0 +1,17 @@
+from yt_comments.analysis.tfidf.models import TfidfKeyword
+from yt_comments.nlp.stopwords import SENTIMENT_EN_VOCABULARY
+
+
+def filter_keywords(keywords: list[TfidfKeyword]) -> list[TfidfKeyword]:
+    out: list[TfidfKeyword] = []
+    
+    for keyword in keywords:
+        is_ngram = " " in keyword.token
+        if not is_ngram and keyword.token in SENTIMENT_EN_VOCABULARY:
+            continue
+        out.append(keyword)
+        
+    return out
+            
+
+    
