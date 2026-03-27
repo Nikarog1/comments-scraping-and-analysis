5cfc5d778bc01c8f4959bb2970dfb6c576ba1894
Nikarog1 <nikita.rogozhnikov@gmail.com>
2026-03-27 18:09:06 +0100

    Refactor channel loader to allow batch loading

--- src/yt_comments/analysis/channel/channel_loader.py
+++ src/yt_comments/analysis/channel/channel_loader.py
@@ -1,4 +1,5 @@
 from typing import Iterator
+import pyarrow.parquet as pq
 
 from yt_comments.storage.silver_comments_repository import ParquetSilverCommentsRepository
 
@@ -7,9 +8,11 @@ class ChannelTextsLoader:
     def __init__(self, silver_repo: ParquetSilverCommentsRepository):
         self._silver_repo = silver_repo
 
-    def iter_texts(self, video_ids: tuple[str, ...]) -> Iterator[str]:
+    def iter_texts(self, video_ids: tuple[str, ...], batch_size: int = 5000) -> Iterator[str]:
         for video_id in video_ids:
-            table = self._silver_repo.load(video_id=video_id)
-            for text in table["text_clean"].to_pylist():
-                if text:
+            path = self._silver_repo._path_for_comments(video_id=video_id)
+            
+            table = pq.ParquetFile(path)
+            for batch in table.iter_batches(batch_size=batch_size, columns=["text_clean"]):
+                for text in batch.column(0).to_pylist():
                     yield text
